baefecbe6e1ca34a4cbe3dff500b88dc3ffe7901
Aleev141 <roman.galeev.2016@mail.ru>
2026-09-05 19:41:57 +0500

    update .22

--- src-en/data/scripts/rkeventeffects.lua
+++ src-en/data/scripts/rkeventeffects.lua
@@ -2,9 +2,9 @@
 script.on_game_event("RKS_UPGRADE_DRONES_BY_2", false, function()
 	Hyperspace.ships.player:GetSystem(4):UpgradeSystem(2)
 end)
---[[ script.on_game_event("RKS_UPGRADE_ENGINES_BY_2", false, function()
+script.on_game_event("RKS_UPGRADE_ENGINES_BY_2", false, function()
 	Hyperspace.ships.player:GetSystem(1):UpgradeSystem(2)
-end) ]]
+end)
 script.on_game_event("RKS_UPGRADE_SHIELDS_BY_2", false, function()
 	Hyperspace.ships.player:GetSystem(0):UpgradeSystem(2)
 end)
@@ -37,6 +37,29 @@ script.on_internal_event(Defines.InternalEvents.GET_DODGE_FACTOR, function(shipM
 	return Defines.Chain.CONTINUE, value
 end)
 ]]
+script.on_internal_event(Defines.InternalEvents.GET_DODGE_FACTOR, function(shipManager, value)
+	if mods.aea then		--Arc's The Outer Expansion ( = the reference) is already doing it.
+		return Defines.Chain.CONTINUE, value
+	end
+
+	if shipManager:HasSystem(1) then
+		local engine = shipManager:GetSystem(1)
+		if engine:GetEffectivePower() >= 9 then
+			local powerExtra = engine:GetEffectivePower() - 8
+			local pilot = shipManager:GetSystem(6)
+			if pilot.bManned then
+				value = value + 35 + (5 * powerExtra)
+			elseif pilot.powerState.first == 2 then
+				value = value + ((35 + (5 * powerExtra)) * 0.5)
+			elseif pilot.powerState.first == 3 then
+				value = value + ((35 + (5 * powerExtra)) * 0.8)
+			end
+		end
+	end
+	return Defines.Chain.CONTINUE, value
+end)
+
+
 
 --Scorch B.I.L.
 script.on_game_event("RKS_UPGRADE_REACTOR_BY_5", false, function()
